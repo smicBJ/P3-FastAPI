ac49cb669421f575a8d1379def8d50cf242cff7b
eThAN02010506 <144652834+eThAN02010506@users.noreply.github.com>
2023-09-26 10:31:14 +0800

    Add files via upload

--- class/3quiz_feedback/5tbe6mP845ZQ/main.py
+++ class/3quiz_feedback/5tbe6mP845ZQ/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+import uvicorn
 # -1: missing uvicorn import
+# added missing import
+from random import randint
+
 # -1: need to import randint from the random library
+# added missing import
+
+
 # Extra note, standard library should be imported before external libraries
-import random
 
 app = FastAPI()
 
@@ -10,7 +16,9 @@ app = FastAPI()
 @app.get("/")
 def home_route():
     # -1: Return a string, not a set with a string inside
-    return {"Unit 3": "Quiz"}
+    # removed the original and change to return a string.
+    # return {"Unit 3": "Quiz"}
+    return "Unit 3 Quiz"
 
 
 @app.get("/about_me")
@@ -21,14 +29,19 @@ def about_me():
 @app.get("/helper/random_number")
 def random_number_helper():
     # -1: missing random_num variable
-    return {"random 1:", random.random()}
+    # added the missing variable
+    # return {"random 1:", random.random()}
+    random_num = randint(0, 100000000000)
 
     # -1: Due to the above return statement, all this code can never be reached and is irrelevant
-    @app.get("/dictionary")
-    def ret_dict():
-        return {"random number": random_number_helper}
+    # removed the below codes.
+    # @app.get("/dictionary")
+    # def ret_dict():
+    # return {"random number": random_number_helper}
 
-    print(random_number_helper)
+    # print(random_number_helper)
+    # change to return
+    return {"number": f"{random_num}"}
 
 
 if __name__ == "__main__":
