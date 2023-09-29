67094fd868e90a20e6490999e3cf568af8d91c9f
Jonathan Fernandes <jhfernan@mail.usf.edu>
2023-09-29 11:59:02 +0800

    Deep dive into route parameters and query params

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-from random import randint
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 
 
-@app.get("/")
-def random_number_helper():
-    random_num = randint(0, 1000000000000000000000000000000)
-    return {"number": random_num}
+# First we make some fake data to be able to use as our "database calls"
+users = [
+    {"name": "Hudson", "grade": 11},
+    {"name": "Jack", "grade": 10},
+    {"name": "Adriaan", "grade": 11}
+]
 
 
-@app.get("/about")
-def read_about():
-    return {"name": "Jon"}
+# Here we see that order matters. Going to this path will trigger this route first rather than the next because it
+# comes first
+@app.get("/users/me")
+async def get_me():
+    return {"name": "My Profile"}
 
 
-@app.get("/users/{user_id}")
-def read_user(user_id: str):
-    return {"id": user_id}
+# Parameters in our route definitions allow us to match multiple paths
+@app.get("/users/{user_name}")
+async def get_profile(user_name: str):
+    for user in users:
+        if user_name.lower() == user["name"].lower():
+            return user
+    return {"msg": "User not found"}
+
+
+# Here we see another way to create dynamic routes using query parameters
+@app.get("/documents")
+async def get_document(name: str | None = None):
+    if name:
+        return {"msg": f"Document {name} successfully retrieved", "doc": name}
+    return {"msg": "Name was not passed as a query"}
 
 
 if __name__ == "__main__":
-    # -1: Missing reload
-    # uvicorn.run("main:app")
     uvicorn.run("main:app", reload=True)
