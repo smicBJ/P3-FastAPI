261cd446198475cbb97c19f7d796afff1c9e7724
Jonathan Fernandes <jhfernan@mail.usf.edu>
2023-10-29 10:25:53 +0800

    Added routers to main and static directory

--- main.py
+++ main.py
@@ -1,32 +1,15 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-from class_info.lessons.opening_files import Helper
+from routers import academic, athlete
 
 app = FastAPI()
-data = Helper()
-API_KEYS = ["pen4hr48snc", "79sfhsieuhsi", "0123456789"]
 
+app.include_router(academic.router, prefix="/academics")
+app.include_router(athlete.router, prefix="/athletes")
 
-@app.get("/athletes")
-async def get_all_athletes(api_key: str):
-    if api_key in API_KEYS:
-        res = []
-        for athlete in data.data:
-            res.append(athlete["name"])
-        return res
-    return {"msg": "You do not have permission to access this rss"}
-
-
-@app.get("/athletes/{athlete}")
-async def get_athlete(athlete: str, api_key: str):
-    if api_key in API_KEYS:
-        for student in data.data:
-            if athlete in student["name"]:
-                return student
-        return {"msg": "Athlete not found"}
-    return {"msg": "You do not have permission to access this rss"}
-
+app.mount("/static", StaticFiles(directory="static"))
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True)
